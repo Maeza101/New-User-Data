956cea8e7acb9444d1608b729c68e913350eea5d
Maeza101 <makheler@caa.co.za>
2025-06-16 15:48:51 +0200

    New commit_160625

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateUser from "./pages/CreateUser";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/CreateUser" element={<CreateUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/CreateUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateUser = () => {
+    return (
+        <div>
+            <h1>This is the create user page</h1>
+        </div>
+    )
+}
+
+export default CreateUser;
